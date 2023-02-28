155cf82f2168c0439f2630a48c5f78f9e1f071e6
Praveen kumar G <110656664+praveen-all@users.noreply.github.com>
2023-02-28 23:17:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function func(){
+    let val="0123456789ABCDEF";
+    let cons="#";
+    for(let i=0;i<6;i++){
+        cons+=val[Math.round(Math.random()*(15-0)+0)];
+    }
+    return cons;
+}
+
+let btn=document.getElementById("btn1");
+let div=document.querySelector(".clrbdy");
+// console.log(div);
+// console.log(btn);
+let inverval;
+btn.addEventListener("click",()=>{
+    inverval=setInterval(() => {
+    let app=func();
+    div.style. backgroundColor=app;
+    div.innerText=app;
+  }, 1000);
+})
+
+
+let btn1=document.getElementById('btn2');
+btn1.addEventListener("click",()=>{
+    div.style. backgroundColor="white";
+    clearInterval(inverval);
+})
